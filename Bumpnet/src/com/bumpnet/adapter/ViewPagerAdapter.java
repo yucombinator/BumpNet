df420446a9d7eae8b24ee52ba008c858ab90afe2
Yu Chen Hou <houyuchen66@gmail.com>
2013-06-14 23:47:33 -0400

    initial commit with template

---
+++ Bumpnet/src/com/bumpnet/adapter/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+
+package com.bumpnet.adapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.bumpnet.SampleFragment_;
+
+public class ViewPagerAdapter
+    extends FragmentPagerAdapter
+{
+
+    private String[] locations;
+
+    public ViewPagerAdapter(FragmentManager fm, String[] locations) {
+        super(fm);
+        this.locations = locations;
+    }
+
+    public int getCount() {
+        return locations.length;
+    }
+
+    public Fragment getItem(int position) {
+        Fragment fragment = new SampleFragment_();
+        Bundle bundle = new Bundle();
+        bundle.putString("label", locations[position]);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+}
