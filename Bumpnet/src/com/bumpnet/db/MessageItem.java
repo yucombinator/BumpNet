fea58104c220ccaaa84c5328db1f842f0903a4af
Julian C. Osorio <juliancamilo.osorio@gmail.com>
2013-06-25 15:06:35 -0400

    Added SQLite database to store messages
    Implemented MessageOpenHelper to manage the SQLDatabase
    and filled the MessageItem class to allow access to the database.

--- Bumpnet/src/com/bumpnet/db/MessageItem.java
+++ Bumpnet/src/com/bumpnet/db/MessageItem.java
@@ -1,11 +1,88 @@
 package com.bumpnet.db;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
 
 public class MessageItem{
 	public Drawable image;
 	public String title;
-	public String message;
+	private String m_message;
+	private int m_parentId;
+	private int m_id;
+
+	MessageItem(Context context, int id) {
+		m_id = id;
+		MessageOpenHelper db = new MessageOpenHelper(context);
+		
+		SQLiteDatabase readableDB = db.getReadableDatabase();
+		String[] selection = {"message_text", "parent_id"};
+		String[] selectionArgs = {"" + m_id};
+		Cursor matches = readableDB.query(MessageOpenHelper.MESSAGES_TABLE_NAME, selection,
+				"id = ?", selectionArgs, null, null, null, null);
+		m_message = matches.getString(matches.getColumnIndex("message_text"));
+		if(matches.isNull(matches.getColumnIndex("parent_id"))){
+			m_parentId = -1;
+		} else {
+			m_parentId = matches.getInt(matches.getColumnIndex("parent_id"));
+		}
+		matches.close();
+		readableDB.close();
+	}
+	
+	private MessageItem(Context context, int id, String message_text, int parentId) {
+		m_id = id;
+		m_message = message_text;
+		m_parentId = parentId;
+	}
+	
+	public MessageItem[] getReplies(Context context) {
+		MessageOpenHelper db = new MessageOpenHelper(context);
+		
+		SQLiteDatabase readableDB = db.getReadableDatabase();
+		String[] selection = {"id", "message_text", "parent_id"};
+		String[] selectionArgs = {"" + m_id};
+		Cursor matches = readableDB.query(MessageOpenHelper.MESSAGES_TABLE_NAME, selection,
+				"parent_id = ?", selectionArgs, null, null, null, null);
+		
+		int replyCount = matches.getCount();
+		int i = 0;
+		MessageItem[] replies = new MessageItem[replyCount];
+		do {
+			replies[i] = new MessageItem(context, matches.getInt(0), matches.getString(1), matches.getInt(2));
+			i++;
+		} while(matches.moveToNext());
+		matches.close();
+		readableDB.close();
+		
+		return replies;
+	}
+	
+	public static MessageItem[] getTopLevelMessages(Context context) {
+		MessageOpenHelper db = new MessageOpenHelper(context);
+		
+		SQLiteDatabase readableDB = db.getReadableDatabase();
+		String[] selection = {"id"};
+		Cursor matches = readableDB.query(MessageOpenHelper.MESSAGES_TABLE_NAME, selection,
+				"parent_id IS NULL", new String[] {}, null, null, null, null);
+		
+		int topCount = matches.getCount();
+		int i = 0;
+		MessageItem[] topLevelMessages = new MessageItem[topCount];
+		do {
+			topLevelMessages[i] = new MessageItem(context, matches.getInt(0));
+			i++;
+		} while(matches.moveToNext());
+		matches.close();
+		readableDB.close();
+		
+		return topLevelMessages;
+	}
+	
+	public int getParentId() {
+		return m_parentId;
+	}
 	
 	public Drawable getIcon(){
 		return image;
@@ -16,7 +93,7 @@ public class MessageItem{
 	}
 	
 	public String getMessage(){
-		return message;
+		return m_message;
 	}
 	
 }
---
+++ Bumpnet/src/com/bumpnet/db/MessageOpenHelper.java
@@ -0,0 +1,38 @@
+package com.bumpnet.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class MessageOpenHelper extends SQLiteOpenHelper {
+
+	private static final String TAG = "MessageOpenHelper"; //For log messages
+	
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "bumpnetMessageDB";
+	public static final String MESSAGES_TABLE_NAME = "messages";
+	private static final String CREATE_MESSAGE_DATABASE = "CREATE TABLE " + MESSAGES_TABLE_NAME + "(" +
+			"id INTEGER PRIMARY KEY, " +
+			"message_text TEXT, " +
+			"parent_id INTEGER" + ");";
+	
+	public MessageOpenHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_MESSAGE_DATABASE);
+		Log.v(TAG, "Database created");
+	}
+
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(TAG, "Requested database upgrade from version" + oldVersion + " to " + newVersion);
+		Log.w(TAG, "This Database has only one known version");
+	}
+
+}
